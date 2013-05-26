ac09cf39cd0cbb504c976849396d2f81302da602
Shinji Kaneko <contact@kanekoelastic.org>
2013-05-26 14:51:22 +0900

    .vimperatorrc追加

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MAKE_LINK_LIST=".vimrc .vim .bashrc .zshrc .gitconfig"
+MAKE_LINK_LIST=".vimrc .vim .bashrc .zshrc .gitconfig .vimperatorrc"
 cd $(dirname $0)
 for dotfile in $MAKE_LINK_LIST
 do

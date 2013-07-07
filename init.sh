95849f3bcb0a53f8fa2dc00c0ae7a5f650282a60
Shinji Kaneko <contact@kanekoelastic.org>
2013-07-07 12:58:31 +0900

    Make Mac dir

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MAKE_LINK_LIST=".vimrc .vim .bashrc .zshrc .gitconfig .vimperatorrc .slate.js"
+MAKE_LINK_LIST=".vimrc .vim .bashrc .zshrc .gitconfig .vimperatorrc"
 cd $(dirname $0)
 for dotfile in $MAKE_LINK_LIST
 do
@@ -8,6 +8,3 @@ done
 
 # git がインストールされたら、git complehentionを持ってくる
 cp /etc/bash_completion.d/git $HOME/.git-completion.sh
-
-# macの場合は、KeyRemap4MacBookの設定もする
-# ln -s $PWD/private.xml $HOME/Library/Application\ Support/KeyRemap4MacBook/

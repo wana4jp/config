42277f24488f459ad9c717f7772ea63ea531ea98
Shinji Kaneko <contact@kanekoelastic.org>
2013-07-17 23:15:27 +0900

    add _gvimrc

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MAKE_LINK_LIST=".vimrc .vim .bashrc .zshrc .gitconfig .vimperatorrc"
+MAKE_LINK_LIST=".vimrc  _gvimrc .vim .bashrc  .zshrc .gitconfig .vimperatorrc"
 cd $(dirname $0)
 for dotfile in $MAKE_LINK_LIST
 do

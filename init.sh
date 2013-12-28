06a6c1128f8fe3d4e626160289a7b70d6730956e
Shinji Kaneko <contact@kanekoelastic.org>
2013-12-28 11:56:24 +0900

    create dotfiles dir

--- init.sh
+++ init.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
-MAKE_LINK_LIST=".vimrc  _gvimrc .vim .bashrc  .zshrc .gitconfig .vimperatorrc"
+
+MAKE_LINK_LIST=".vimrc .gvimrc .vim"
 cd $(dirname $0)
 for dotfile in $MAKE_LINK_LIST
 do
 	ln -Fis "$PWD/$dotfile" $HOME
 done
 
+
+MAKE_LINK_LIST=".bashrc .zshrc .gitconfig"
+
+cd $(dirname $0)
+for dotfile in $MAKE_LINK_LIST
+do
+	ln -Fis "$PWD/dotfiles/$dotfile" $HOME
+done
+
 source ~/.bashrc
-mkdir ~/vswap
+if [ ! -d ~/vswap ]; then
+    mkdir ~/vswap
+fi

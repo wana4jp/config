139755638cb5fdef8efa5258086b5b66cecc584b
Shinji Kaneko <contact@kanekoelastic.org>
2014-01-09 14:34:37 +0900

    edit slate.js

--- mac/.slate.js
+++ mac/.slate.js
@@ -9,7 +9,7 @@ var superKey = ':alt,cmd,shift,ctrl';
 // ---------
 var util = {
   eachAppWindow: function(f) {
-    slate.eachApp(function(app) {
+    S.eachApp(function(app) {
         app.eachWindow(f);
     });
   },
@@ -28,7 +28,7 @@ var util = {
   },
   focusWindow: function(f) {
     var hit = false;
-    slate.eachApp(function(app) {
+    S.eachApp(function(app) {
         if (hit) return;
         app.eachWindow(function(win) {
             if (hit) return;
@@ -40,7 +40,7 @@ var util = {
     });
   },
   nextScreen: function(screen) {
-    return slate.screenForRef(String( (screen.id()+1)%slate.screenCount() ));
+    return S.screenForRef(String( (screen.id()+1)%S.screenCount() ));
   }
 };
 
@@ -49,19 +49,19 @@ var util = {
 // --------
 // bindings
 // --------
-slate.bind(util.key('space'), slate.operation('focus', {
+S.bind(util.key('space'), S.operation('focus', {
   direction: 'behind'
 }));
 
-slate.bind(util.key('9'), function(win) {
+S.bind(util.key('9'), function(win) {
   if (!win) return;
   var next = util.nextScreen(win.screen());
   win.move(next.visibleRect());
 });
 
-slate.bind(util.key('down'), slate.operation('chain', {
+S.bind(util.key('down'), S.operation('chain', {
   operations: _.map(['top-right', 'bottom-right', 'bottom-left', 'top-left'], function(d) {
-    return slate.operation('corner', {
+    return S.operation('corner', {
       direction: d,
       width: 'screenSizeX/2',
       height: 'screenSizeY/2'
@@ -69,48 +69,48 @@ slate.bind(util.key('down'), slate.operation('chain', {
   })
 }));
 
-slate.bind(util.key('up'), function(win) {
+S.bind(util.key('up'), function(win) {
   if (!win) return;
   var bounds = win.screen().visibleRect();
   win.doOperation('move', bounds);
 });
 
-slate.bind(util.key('left'), slate.operation('push', {
+S.bind(util.key('left'), S.operation('push', {
   direction: 'left',
   style: 'bar-resize:screenSizeX/2'
 }));
 
-slate.bind(util.key('right'), slate.operation('push', {
+S.bind(util.key('right'), S.operation('push', {
   direction: 'right',
   style: 'bar-resize:screenSizeX/2'
 }));
 
-slate.bind(util.key('h'), slate.operation('focus', { direction: 'left' }));
-slate.bind(util.key('j'), slate.operation('focus', { direction: 'down' }));
-slate.bind(util.key('k'), slate.operation('focus', { direction: 'up' }));
-slate.bind(util.key('l'), slate.operation('focus', { direction: 'right' }));
+S.bind(util.key('h'), S.operation('focus', { direction: 'left' }));
+S.bind(util.key('j'), S.operation('focus', { direction: 'down' }));
+S.bind(util.key('k'), S.operation('focus', { direction: 'up' }));
+S.bind(util.key('l'), S.operation('focus', { direction: 'right' }));
 
 
 // -------
 // layouts
 // -------
 
-var focusITerm = slate.operation("focus", { "app" : "iTerm" });
+var focusITerm = S.operation("focus", { "app" : "iTerm" });
 
-var leftBottomLeft = slate.operation("move", {
-    "x" : "screenOriginX",
-    "y" : "screenOriginY+(screenSizeY/2)",
-    "width" : "screenSizeX/2",
-    "height": "screenSizeY/2"
-  });
-
-var firstMonitorLayout = slate.layout("threeMonitors", {
-    "_after_" : {
-      "operations" : focusITerm
-    },
-   "MacVim" : {
-    "operations" : leftBottomLeft
-    }
+var leftBottomLeft = S.operation("move", {
+  "x" : "screenOriginX",
+  "y" : "screenOriginY+(screenSizeY/2)",
+  "width" : "screenSizeX/2",
+  "height": "screenSizeY/2"
 });
 
-slate.bind(util.key('a'), slate.operation("layout", {"name" : firstMonitorLayout}));
+var firstMonitorLayout = S.layout("threeMonitors", {
+  "_after_" : {
+    "operations" : focusITerm
+  },
+ "MacVim" : {
+    "operations" : leftBottomLeft
+  }
+});
+
+S.bind(util.key('a'), S.operation("layout", {"name" : firstMonitorLayout}));

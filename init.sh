82c2f9d667b2b80d27323a1ab97060a94bd4e834
Shinji Kaneko <contact@kanekoelastic.org>
2016-02-08 12:35:46 +0900

    add .ideavimrc

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAKE_LINK_LIST=".bashrc .bash_profile .tmux.conf .zshrc .gitconfig .vimrc .gvimrc .vim .ctags"
+MAKE_LINK_LIST=".bashrc .bash_profile .tmux.conf .zshrc .gitconfig .vimrc .gvimrc .vim .ctags .ideavimrc"
 
 cd $(dirname $0)
 for dotfile in $MAKE_LINK_LIST

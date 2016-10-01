87159588cedecb21ab54b0387a69ea3dfd12d62e
Shinji Kaneko <contact@kanekoelastic.org>
2016-10-02 00:58:39 +0900

    add precommit fook for git

--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-MAKE_LINK_LIST=".bashrc .bash_profile .tmux.conf .zshrc .gitconfig .vimrc .gvimrc .ideavimrc .vim .ctags"
-
 cd $(dirname $0)
-for dotfile in $MAKE_LINK_LIST
+for dotfile in `ls -A dotfiles/`
 do
 	ln -Fis "$PWD/dotfiles/$dotfile" $HOME
 done
 
-source ~/.bashrc
 if [ ! -d ~/vimswap ]; then
     mkdir ~/vimswap
 fi
+
+# copy fook scripts when create new git repository 
+git config --global init.templatedir ~/.git_template

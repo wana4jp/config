a61eae0681183ab1e95f7852d764c928e69b5a2e
Shinji Kaneko <contact@kanekoelastic.org>
2016-10-25 21:55:54 +0900

    rename mac init sh and tweak

---
+++ env/macOS/init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+MAKE_LINK_LIST=".slate.js"
+for dotfile in $MAKE_LINK_LIST
+do
+	ln -Fis "$PWD/$dotfile" "$HOME"
+done
+
+# KeyRemap4MacBook
+ln -Fis "$PWD/private.xml" "$HOME/Library/Application Support/Karabiner"

efcc58ca4b68d2635566a23954e5f8f92c92cea3
Shinji Kaneko <contact@kanekoelastic.org>
2017-11-04 21:56:57 +0900

    edit setup.sh

--- init.sh
+++ init.sh
@@ -2,12 +2,17 @@
 
 for dotfile in `ls -A dotfiles/`
 do
-	ln -Fis "$PWD/dotfiles/$dotfile" "$HOME"
+   ln -Fis "$PWD/dotfiles/$dotfile" "$HOME"
 done
 
 if [ ! -d ~/vimswap ]; then
     mkdir ~/vimswap
 fi
 
+if [ ! -d ~/.ssh ]; then
+    mkdir ~/.ssh
+    touch ~/.ssh/config
+    chmod 600 ~/.ssh/config
+fi
 # copy fook scripts when create new git repository
 git config --global init.templatedir ~/.git_template

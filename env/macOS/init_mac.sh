439dcb1be8947ee06cf1848396c2ddbf2cfde9bd
Shinji Kaneko <shinjikaneko@wana4-iMac.lan>
2020-02-14 13:27:50 +0900

    Add VScode Config

--- env/macOS/init_mac.sh
+++ env/macOS/init_mac.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
-ln -Fis ~/config/env/macOS/karabiner/my_conf.json ~/.config/karabiner/assets/complex_modifications/
+ln -Fis ~/config/env/macOS/karabiner/my_conf.json ~/.config/karabiner/assets/complex_modifications
 ln -Fis ~/config/env/macOS/.Brewfile ~/.Brewfile
+
+
+VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+
+rm "$VSCODE_SETTING_DIR/settings.json"
+ln -Fis ~/config/settings.json "${VSCODE_SETTING_DIR}/settings.json"
+
+rm "$VSCODE_SETTING_DIR/keybindings.json"
+ln -Fis ~/config/keybindings.json "${VSCODE_SETTING_DIR}/keybindings.json"
+
+cat ~/config/vscode_extensions | while read line
+do
+ code --install-extension $line
+done
+
+code --list-extensions > ~/config/vscode_extensions
